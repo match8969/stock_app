e03edbe58d385d432abd98dceb1a10b7e52881b6
町田龍浩 <match8969@gmail.com>
2019-08-06 18:11:24 +0900

    Add root page and header with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # Root
+  root :to => "root#index"
+  
+  get 'root/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
