0360f100ce81aafc6c7bb632f79c89d952005e67
町田龍浩 <match8969@gmail.com>
2019-08-09 18:47:27 +0900

    update #8

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@ class Company < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :holdings, dependent: :destroy
   has_one :stock, dependent: :destroy
+  belongs_to :country
 end
