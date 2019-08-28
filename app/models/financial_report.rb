9fec69793a36af9830a4f967347887b2ee29cb55
町田龍浩 <match8969@gmail.com>
2019-08-28 21:12:33 +0900

    update

--- app/models/financial_report.rb
+++ app/models/financial_report.rb
@@ -1,3 +1,10 @@
 class FinancialReport < ApplicationRecord
   belongs_to :company
+
+  private
+    def to_date(report_date)
+      # TODO: ここにスクレイピングして取得した日付を
+      # datetimeに変更する処理
+
+    end
 end
